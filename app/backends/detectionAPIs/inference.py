53e1a504ad14b71875d8f3ab7cf0cd348d1fbe56
JerryMa <jerry_mazeyu@163.com>
2025-05-30 13:45:37 +0800

    Add: Initialization

---
+++ app/backends/detectionAPIs/inference.py
@@ -0,0 +1,10 @@
+def inference(model, image_path):
+    pass
+
+def load_model(model_path):
+    pass
+
+def load_image(image_path):
+    pass
+
+
---
+++ app/backends/monitorAPIs/monitor.py
@@ -0,0 +1,8 @@
+def feature_extraction(image_path):
+    pass
+
+def PID_control(image_path):
+    pass
+
+def result_analysis(image_path):
+    pass
